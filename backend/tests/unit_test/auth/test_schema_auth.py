5fc07428f270162360f9480b027a3312ff989de4
YvanMartineau <martineaubadou9@gmail.com>
2026-08-29 21:48:01 +0200

    fix(ci): Backend CI unit_test

--- backend/tests/unit_test/auth/test_schema_auth.py
+++ backend/tests/unit_test/auth/test_schema_auth.py
@@ -3,10 +3,10 @@ from pydantic import ValidationError
 from schemas.auth import LoginRequest, TokenResponse
 
 
-TEST_PASSWORD = "securepass"
-TEST_ACCESS_TOKEN = "abc"
-TEST_DEFAULT_TOKEN_TYPE = "bearer"
-TEST_TOKEN = "token123"
+TEST_PASSWORD = "securepass" # noqa: S106 # nosec
+TEST_ACCESS_TOKEN = "abc" # noqa: S106 # nosec
+TEST_DEFAULT_TOKEN_TYPE = "bearer" # noqa: S106 # nosec
+TEST_TOKEN = "token123" # noqa: S106 # nosec
 
 
 class TestLoginRequest:
